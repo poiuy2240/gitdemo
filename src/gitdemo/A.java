3e0fcb2cfc05362c95ed367ff9ab5869b2ae072a
poiuy2240 <poiuy2240@126.com>
2015-07-06 17:57:32 +0800

    xx

--- src/gitdemo/A.java
+++ src/gitdemo/A.java
@@ -3,7 +3,7 @@ package gitdemo;
 public class A {
 
 	public static void main(String[] args) {
-		System.out.println();
+		System.out.println(1231);
 	}
 	public void b(){
 		System.out.println(1231);
