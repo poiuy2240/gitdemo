b8dab91edd8899ca78569151ea982a9b90943dac
poiuy2240 <poiuy2240@126.com>
2015-07-06 15:12:01 +0800

    Update A.java

--- src/gitdemo/A.java
+++ src/gitdemo/A.java
@@ -5,8 +5,8 @@ public class A {
 	public static void main(String[] args) {
 		System.out.println();
 	}
-	public void a(){
-		System.out.println(1231);
+	public void b(){
 		System.out.println(1231);
 	}
 }
+
