88261778203db4d97635291296b96f9e8c4e495e
poiuy2240 <poiuy2240@126.com>
2015-07-06 15:28:58 +0800

    x

--- src/gitdemo/A.java
+++ src/gitdemo/A.java
@@ -5,9 +5,11 @@ public class A {
 	public static void main(String[] args) {
 		System.out.println();
 	}
-	public void a(){
+	public void b(){
 		System.out.println(1231);
 		System.out.println(22);
+		
+		
 	}
 }
 
